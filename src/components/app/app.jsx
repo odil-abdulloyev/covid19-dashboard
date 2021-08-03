b07457461e59cb45f93e5db4cccdd6d80d0abbeb
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-08-04 00:28:53 +0500

    feat: implement spinner

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { Layout } from '../layout/layout';
@@ -8,16 +8,26 @@ import { Map } from '../map/map';
 import { Chart } from '../chart/chart';
 import { setCovidData } from '../../redux/actions/actions';
 import { config } from '../../config';
+import { Spinner } from '../spinner/spinner';
 
 export const App = () => {
+  const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(config.apiUrl);
       return await response.data;
     };
-    fetchData().then((data) => dispatch(setCovidData(data)));
+    fetchData().then((data) => {
+      dispatch(setCovidData(data));
+      setLoaded(true);
+    });
   }, [dispatch]);
+
+  if (!loaded) {
+    return <Spinner />;
+  }
+
   return (
     <Layout
       listContent={<List />}
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,13 @@
+import './spinner.css';
+
+export const Spinner = () => {
+  return (
+    <div className='spinner'>
+      <div className='loadingio-spinner-rolling-tgnvbc7dmop'>
+        <div className='ldio-gb52ke7jo7'>
+          <div />
+        </div>
+      </div>
+    </div>
+  );
+};
