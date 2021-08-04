1df4603aed87e91af7dc5db66cbc8ca94fa1d6e3
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-08-04 11:32:17 +0500

    docs: update readme

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export const config = {
   apiUrl: 'https://corona.lmao.ninja/v2/countries',
   mapTileLayerUrl:
-    'https://api.mapbox.com/styles/v1/abdulloh76/ckiuab9eh2phd19qk49l16mvl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWJkdWxsb2g3NiIsImEiOiJja2l1YTV6Z3EwOWdhMnpuenE3N3gxOHhjIn0.txVoKFibwsl1bMD2ylfxOA',
-  // https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png
+    'https://api.mapbox.com/styles/v1/j0hn-d03/ckrx3ina30ihn18p683g9oyn1/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiajBobi1kMDMiLCJhIjoiY2tyeDJ1d2IyMDUzNjJvbXJjaTVuNjR1biJ9.zspgA8Q6Df0zjna6OS4QJg',
   index: {
     cases: 'cases',
     deaths: 'deaths',
